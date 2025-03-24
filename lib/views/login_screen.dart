3229e5e05d96d8e9ba81d9b83e286a081765183d
palcode1 <arkanantanaufal@gmail.com>
2025-03-24 14:18:30 +0700

    add_images

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -34,7 +34,13 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(color: Colors.teal.shade200),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFB1F0F7), Color(0xFF81BFDA)],
+          ),
+        ),
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
@@ -42,57 +48,67 @@ class _LoginScreenState extends State<LoginScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset('assets/images/hospital_logo.png', height: 100),
+                Image.asset('assets/images/clinic_logo.png', height: 100),
                 SizedBox(height: 20),
                 Text(
                   'Selamat Datang',
                   style: TextStyle(fontSize: 24, color: Colors.black),
                 ),
                 SizedBox(height: 60),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Email',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 5),
-                    TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        hintText: 'Masukkan Email',
-                        hintStyle: TextStyle(
-                          color: Colors.grey.shade800.withOpacity(1),
+                Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  color: Color(0xF5F0CD),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Email',
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        filled: true,
-                        fillColor: Colors.grey.shade500.withOpacity(0.3),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                        SizedBox(height: 5),
+                        TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            hintText: 'Masukkan Email',
+                            hintStyle: TextStyle(
+                              color: Colors.grey.shade800.withOpacity(1),
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey.shade500.withOpacity(0.3),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    Text(
-                      'Password',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 5),
-                    TextField(
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        hintText: 'Masukkan Password',
-                        hintStyle: TextStyle(
-                          color: Colors.grey.shade800.withOpacity(1),
+                        SizedBox(height: 15),
+                        Text(
+                          'Password',
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        filled: true,
-                        fillColor: Colors.grey.shade500.withOpacity(0.3),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                        SizedBox(height: 5),
+                        TextField(
+                          controller: passwordController,
+                          decoration: InputDecoration(
+                            hintText: 'Masukkan Password',
+                            hintStyle: TextStyle(
+                              color: Colors.grey.shade800.withOpacity(1),
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey.shade500.withOpacity(0.3),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          obscureText: true,
                         ),
-                      ),
-                      obscureText: true,
+                      ],
                     ),
-                  ],
+                  ),
                 ),
                 SizedBox(height: 20),
                 SizedBox(
